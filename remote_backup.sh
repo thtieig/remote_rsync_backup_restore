b2ba188f0128fa962a20db92dab2392dde9fecaf
ChrisTian <10831582+thtieig@users.noreply.github.com>
2025-05-04 11:31:35 +0100

    reworked remote_Backup.sh

--- remote_backup.sh
+++ remote_backup.sh
@@ -5,15 +5,16 @@
 # - description  : Bash script that rsync's the content of a server(s) to a
 #                  local folder. Using RSYNC you can restore the entire server
 #                  to a new machine in one single run.
+#                  It can run PRE and POST scripts, if some actions are 
+#                  required before or after the rsync.
 # - License      : GPLv3
 # - author       : Chris Tian
-# - date         : 2024-02-29
-# - version      : 2.0
+# - date         : 2025-05-05
+# - version      : 3.0
 # - usage        : bash remote_backup.sh
 # - OS Supported : Ubuntu, Debian, SUSE, Gentoo, RHEL, CentOS, Scientific, Arch
-# - Credits      : Extracted from https://github.com/cloudnull/InstanceSync
+# - Credits      : Ispired by https://github.com/cloudnull/InstanceSync
 # =============================================================================
-
 
 # ============
 # REQUIREMENTS
@@ -66,87 +67,88 @@ RSYNC_FLAGS='aHEAXSzx --delete -v'
 #RSYNC_FLAGS='av --delete'
 #RSYNC_FLAGS='aHEAXSzx --progress'
 
+# Maximum number of retries before giving up
+MAX_RETRIES=5
+
 ###############################################################################
 
 # NO NEED TO TOUCH BELOW HERE #
 
-# Setting to use RSYNC version 3
-RSYNC="$(which rsync)"
+generate_exclude_list() {
+  # Append log folder to the EXCLUDE_LIST if flag is set to true
+  if $VAR_LOG_TREE_ONLY; then
+    EXCLUDE_LIST="${EXCLUDE_LIST} ${VAR_LOG_PATH}"
+  fi
 
+  EXCLUDE_FILE='/tmp/remote_backup_excludeme.file'
 
-function GENEXCLUDELIST() {
-
-# Append log folder to the EXCLUDE_LIST if flag is set to true
-if ${VAR_LOG_TREE_ONLY} ; then
-  EXCLUDE_LIST="${EXCLUDE_LIST} ${VAR_LOG_PATH}"  
-fi
-
-EXCLUDE_FILE='/tmp/remote_backup_excludeme.file'
-
-echo "${EXCLUDE_LIST}" | tr " " "\n" | sed '/^\s*$/d' > "${EXCLUDE_FILE}"
-
-sleep 2
+  echo "${EXCLUDE_LIST}" | tr " " "\n" | sed '/^\s*$/d' > "${EXCLUDE_FILE}"
 }
 
-
-function RUNRSYNCCOMMAND() {
-  set +e
-  MAX_RETRIES=${MAX_RETRIES:-5}
-  RETRY_COUNT=0
+run_rsync_command() {
+  local REMOTE_SERVER=$1
+  local BACKUP_LOC_PATH=$2
 
   # Make sure destination path exists - if not, create
-  [ -d ${BACKUP_LOC_PATH} ] || mkdir -p ${BACKUP_LOC_PATH}
+  mkdir -p "${BACKUP_LOC_PATH}" || {
+    echo "Error creating directory: ${BACKUP_LOC_PATH}"
+    exit 1
+  }
 
   echo "Performing the rsync copy from ${REMOTE_SERVER} to ${BACKUP_LOC_PATH}"
   
-  # Set the initial return value to failure
-  false
-
-  while [ $? -ne 0 -a ${RETRY_COUNT} -lt ${MAX_RETRIES} ];do
-    RETRY_COUNT=$((${RETRY_COUNT}+1))
+  local retry_count=0
+  while [ $retry_count -lt $MAX_RETRIES ]; do
+    rsync -${RSYNC_FLAGS} --exclude-from="${EXCLUDE_FILE}" root@${REMOTE_SERVER}:/ "${BACKUP_LOC_PATH}/"
     
-    echo "Running: ${RSYNC} -${RSYNC_FLAGS} --exclude-from=\"${EXCLUDE_FILE}\" \
-                                        root@${REMOTE_SERVER}:/ ${BACKUP_LOC_PATH}/"
-    ${RSYNC} -${RSYNC_FLAGS} --exclude-from="${EXCLUDE_FILE}" \
-					root@${REMOTE_SERVER}:/ ${BACKUP_LOC_PATH}/
-    
-    if ${VAR_LOG_TREE_ONLY} ; then
-      ${RSYNC} -${RSYNC_FLAGS}  -f"+ */" -f"- *" root@${REMOTE_SERVER}:${VAR_LOG_PATH}/ ${BACKUP_LOC_PATH}${VAR_LOG_PATH}/
+    if $VAR_LOG_TREE_ONLY; then
+      rsync -${RSYNC_FLAGS} -f"+ */" -f"- *" root@${REMOTE_SERVER}:${VAR_LOG_PATH}/ "${BACKUP_LOC_PATH}${VAR_LOG_PATH}/"
     fi
 
+    if [ $? -eq 0 ]; then
+      break
+    fi
+
+    retry_count=$((retry_count + 1))
     sleep 2
-    echo "Copy completed."
   done
 
-  if [ ${RETRY_COUNT} -ge ${MAX_RETRIES} ];then
+  if [ $retry_count -ge $MAX_RETRIES ]; then
     echo "ERROR: Hit maximum number of retries, giving up."
     echo "$0 script wasn't unable to complete. Please manually check" | mail -s "$0 ERROR" root
     exit 1
   fi
-
-  unset MAX_RETRIES
-  set -e
 }
 
-# -----------------------------------------------------------------------------------------------------------------
-# If PRE_EXEC_SCRIPT is set, execute the script
-[ ! -z "$PRE_EXEC_SCRIPT" ] && bash "$PRE_EXEC_SCRIPT"
+execute_pre_script() {
+  if [ -n "$PRE_EXEC_SCRIPT" ]; then
+    bash "$PRE_EXEC_SCRIPT"
+  fi
+}
 
-# Exclude list generated - same for all servers
-GENEXCLUDELIST
+execute_post_script() {
+  if [ -n "$POST_EXEC_SCRIPT" ]; then
+    bash "$POST_EXEC_SCRIPT"
+  fi
+}
 
-for server in $(echo "${REMOTE_SERVERS_LIST}") ; do
+# Generate exclude list
+generate_exclude_list
+
+# Execute pre script
+execute_pre_script
+
+for server in $REMOTE_SERVERS_LIST; do
   REMOTE_SERVER=$server
   BACKUP_LOC_PATH="${BKP_MAIN_PATH}/${REMOTE_SERVER}/${BKP_REL_PATH}"
   echo "\n####################################################################################################"
-  echo  ">>>> Running backup of $REMOTE_SERVER <<<<"
+  echo ">>>> Running backup of $REMOTE_SERVER <<<<"
   echo "DEBUG: REMOTE_SERVER=$REMOTE_SERVER and BACKUP_LOC_PATH=$BACKUP_LOC_PATH"
-  RUNRSYNCCOMMAND
+  run_rsync_command "$REMOTE_SERVER" "$BACKUP_LOC_PATH"
 done
 
 # Clean up
 rm -f "${EXCLUDE_FILE}"
 
-# If POST_EXEC_SCRIPT is set, execute the script
-[ ! -z "$POST_EXEC_SCRIPT" ] && bash "$POST_EXEC_SCRIPT"
-
+# Execute post script
+execute_post_script
